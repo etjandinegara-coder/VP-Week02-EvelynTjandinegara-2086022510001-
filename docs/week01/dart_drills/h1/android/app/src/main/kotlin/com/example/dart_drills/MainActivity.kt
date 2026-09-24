f029ec4322340dec9c4eb2c1b02b6905d18c8aaf
etjandinegara-coder <etjandinegara@student.ciputra.ac.id>
2026-09-24 22:20:12 +0800

    chore: remove Week 1 files from Week 2 repository

--- docs/week01/dart_drills/h1/android/app/src/main/kotlin/com/example/dart_drills/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dart_drills
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- docs/week01/warung_digital/android/app/src/main/kotlin/com/example/warung_digital/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.warung_digital
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
